02574031d88f195c34a38ade87e7cf665f1e0639
Leonid Trofymchuk <leonid.trofymchuk@workdayinternal.com>
2023-10-06 14:23:20 +0300

    Update deps

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -4,128 +4,126 @@ const updateCallbackMock = jest.fn();
 const updateFrameRateMock = jest.fn();
 const updateAnimationMock = jest.fn();
 
-const createFrameRateManagerMock = jest.fn((): FrameRateManager => ({
+const createFrameRateManagerMock = jest.fn(
+  (): FrameRateManager => ({
     updateCallback: updateCallbackMock,
     updateFrameRate: updateFrameRateMock,
     updateAnimation: updateAnimationMock,
-}));
-
-jest.mock(
-    "../frameRateManager/FrameRateManager", () => ({
-        ...jest.requireActual<
-            typeof import("../frameRateManager/FrameRateManager")
-        >("../frameRateManager/FrameRateManager"),
-        createFrameRateManager: createFrameRateManagerMock,
-    }),
+  })
 );
+
+jest.mock("../frameRateManager/FrameRateManager", () => ({
+  ...jest.requireActual<typeof import("../frameRateManager/FrameRateManager")>(
+    "../frameRateManager/FrameRateManager"
+  ),
+  createFrameRateManager: createFrameRateManagerMock,
+}));
 
 import * as React from "react";
 import { render } from "@testing-library/react";
 import {
-    withReactFrameRate,
-    useFrameRateManager,
-    BaseUpdateProps,
-    Options,
+  withReactFrameRate,
+  useFrameRateManager,
+  BaseUpdateProps,
+  Options,
 } from "../index";
 
 interface UpdateProps extends BaseUpdateProps {
-    counter: number;
+  counter: number;
 }
 
 const testId = "testComponent";
 
 class TestComponent extends React.Component<UpdateProps> {
-    public render() {
-        return (
-            <div data-testid={testId} >{this.props.counter}</div>
-        );
-    }
+  public render() {
+    return <div data-testid={testId}>{this.props.counter}</div>;
+  }
 }
 
 const options: Options<UpdateProps> = {
-    updateState: jest.fn((state: UpdateProps): UpdateProps => ({ ...state })),
-    frameRate: 1,
+  updateState: jest.fn((state: UpdateProps): UpdateProps => ({ ...state })),
+  frameRate: 1,
 };
 
 describe("react-frame-rate", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    it("should return valid component", () => {
-        const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
-        expect(Animated).toBeDefined();
-        expect(Animated.displayName).toBe("FrameRateComponent");
-    });
+  it("should return valid component", () => {
+    const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
+    expect(Animated).toBeDefined();
+    expect(Animated.displayName).toBe("FrameRateComponent");
+  });
 
-    it("should render child component with original properties", () => {
-        const props: UpdateProps = {
-            counter: 15,
-            isAnimating: false,
-        };
-        const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
-        const { getByTestId, getByText } = render(<Animated {...props} />);
-        expect(createFrameRateManagerMock).toBeCalledTimes(1);
-        expect(updateCallbackMock).toBeCalledTimes(1);
-        expect(updateFrameRateMock).toBeCalledWith(options.frameRate);
-        expect(updateAnimationMock).toBeCalledWith(props.isAnimating);
-        expect(getByTestId(testId)).toBeDefined();
-        expect(getByText(String(props.counter))).toBeDefined();
-    });
+  it("should render child component with original properties", () => {
+    const props: UpdateProps = {
+      counter: 15,
+      isAnimating: false,
+    };
+    const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
+    const { getByTestId, getByText } = render(<Animated {...props} />);
+    expect(createFrameRateManagerMock).toBeCalledTimes(1);
+    expect(updateCallbackMock).toBeCalledTimes(1);
+    expect(updateFrameRateMock).toBeCalledWith(options.frameRate);
+    expect(updateAnimationMock).toBeCalledWith(props.isAnimating);
+    expect(getByTestId(testId)).toBeDefined();
+    expect(getByText(String(props.counter))).toBeDefined();
+  });
 
-    it("should start animation", async () => {
-        const props: UpdateProps = {
-            counter: 0,
-            isAnimating: true,
-        };
-        const nextProps: UpdateProps = {
-            ...props,
-            counter: props.counter + 1,
-        }
-        updateAnimationMock.mockImplementationOnce((isAnimating: boolean) => {
-            if (isAnimating) {
-                options.updateState(nextProps);
-            }
-        });
-        const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
-        render(<Animated {...props} />);
-        expect(updateAnimationMock).toBeCalledTimes(1);
-        expect(updateAnimationMock).toBeCalledWith(props.isAnimating);
-        expect(options.updateState).toBeCalledWith(nextProps);
+  it("should start animation", async () => {
+    const props: UpdateProps = {
+      counter: 0,
+      isAnimating: true,
+    };
+    const nextProps: UpdateProps = {
+      ...props,
+      counter: props.counter + 1,
+    };
+    updateAnimationMock.mockImplementationOnce((isAnimating: boolean) => {
+      if (isAnimating) {
+        options.updateState(nextProps);
+      }
     });
+    const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
+    render(<Animated {...props} />);
+    expect(updateAnimationMock).toBeCalledTimes(1);
+    expect(updateAnimationMock).toBeCalledWith(props.isAnimating);
+    expect(options.updateState).toBeCalledWith(nextProps);
+  });
 
-    it("should call update callback", () => {
-        const props: UpdateProps = {
-            counter: 0,
-            isAnimating: false,
-        };
-        updateCallbackMock.mockImplementationOnce((callback: () => void) => {
-            callback();
-        });
-        const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
-        render(<Animated {...props} />);
-        expect(updateCallbackMock).toBeCalledTimes(1);
-        expect(options.updateState).toBeCalledWith(props);
+  it("should call update callback", () => {
+    const props: UpdateProps = {
+      counter: 0,
+      isAnimating: false,
+    };
+    updateCallbackMock.mockImplementationOnce((callback: () => void) => {
+      callback();
     });
+    const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
+    render(<Animated {...props} />);
+    expect(updateCallbackMock).toBeCalledTimes(1);
+    expect(options.updateState).toBeCalledWith(props);
+  });
 
-    it("should stop animation", () => {
-        const props: UpdateProps = {
-            counter: 0,
-            isAnimating: true,
-        };
-        const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
-        const { rerender } = render(<Animated {...props} />);
-        expect(updateAnimationMock).toBeCalledWith(props.isAnimating);
+  it("should stop animation", () => {
+    const props: UpdateProps = {
+      counter: 0,
+      isAnimating: true,
+    };
+    const Animated = withReactFrameRate<UpdateProps>(options)(TestComponent);
+    const { rerender } = render(<Animated {...props} />);
+    expect(updateAnimationMock).toBeCalledWith(props.isAnimating);
 
-        const nextProps: UpdateProps = {
-            counter: 0,
-            isAnimating: false,
-        };
-        rerender(<Animated {...nextProps} />);
-        expect(updateAnimationMock).toBeCalledWith(nextProps.isAnimating);
-    });
+    const nextProps: UpdateProps = {
+      counter: 0,
+      isAnimating: false,
+    };
+    rerender(<Animated {...nextProps} />);
+    expect(updateAnimationMock).toBeCalledWith(nextProps.isAnimating);
+  });
 
-    it("should re-export useFrameRateManager", () => {
-        expect(useFrameRateManager).toBeDefined();
-    });
+  it("should re-export useFrameRateManager", () => {
+    expect(useFrameRateManager).toBeDefined();
+  });
 });
--- src/frameRateManager/FrameRateManager.ts
+++ src/frameRateManager/FrameRateManager.ts
@@ -1,70 +1,75 @@
 export interface FrameRateManager {
-    updateCallback(callback: () => void): void;
-    updateFrameRate(frameRate: number): void;
-    updateAnimation(isAnimating: boolean): void;
+  updateCallback(callback: () => void): void;
+  updateFrameRate(frameRate: number): void;
+  updateAnimation(isAnimating: boolean): void;
 }
 
 class FrameRateManagerClass implements FrameRateManager {
-    private static oneSec = 1000;
-    private static defaultFrameRate = 60;
+  private static oneSec = 1000;
+  private static defaultFrameRate = 60;
 
-    private static toFixedFrameDuration(frameRate: number) {
-        const precision = Math.pow(10, 5);
-        return Math.floor((FrameRateManagerClass.oneSec / frameRate) * precision) / precision;
+  private static toFixedFrameDuration(frameRate: number) {
+    const precision = Math.pow(10, 5);
+    return (
+      Math.floor((FrameRateManagerClass.oneSec / frameRate) * precision) /
+      precision
+    );
+  }
+
+  private frameId = 0;
+  private frameDuration = FrameRateManagerClass.toFixedFrameDuration(
+    FrameRateManagerClass.defaultFrameRate
+  );
+  private lastFrameTime = 0;
+  private callback: (() => void) | undefined;
+
+  public updateCallback = (callback: () => void) => {
+    this.callback = callback;
+  };
+
+  public updateFrameRate = (frameRate: number) => {
+    this.frameDuration = FrameRateManagerClass.toFixedFrameDuration(frameRate);
+  };
+
+  public updateAnimation = (isAnimating: boolean) => {
+    if (isAnimating) {
+      this.startAnimation();
+    } else {
+      this.stopAnimation();
     }
+  };
 
-    private frameId = 0;
-    private frameDuration = FrameRateManagerClass.toFixedFrameDuration(FrameRateManagerClass.defaultFrameRate);
-    private lastFrameTime = 0;
-    private callback: (() => void) | undefined;
+  private startAnimation = () => {
+    this.lastFrameTime = 0;
+    this.frameId = window.requestAnimationFrame(this.playAnimation);
+  };
 
-    public updateCallback = (callback: () => void) => {
-        this.callback = callback;
+  private playAnimation = (time: number) => {
+    this.update(time);
+    this.frameId = window.requestAnimationFrame(this.playAnimation);
+  };
+
+  private stopAnimation = () => {
+    if (this.frameId) {
+      window.cancelAnimationFrame(this.frameId);
+      this.frameId = 0;
     }
+  };
 
-    public updateFrameRate = (frameRate: number) => {
-        this.frameDuration = FrameRateManagerClass.toFixedFrameDuration(frameRate);
+  private tick = () => {
+    if (this.callback) {
+      this.callback();
     }
+  };
 
-    public updateAnimation = (isAnimating: boolean) => {
-        if (isAnimating) {
-            this.startAnimation();
-        } else {
-            this.stopAnimation();
-        }
+  private update = (time: number) => {
+    if (time - this.lastFrameTime >= this.frameDuration) {
+      this.tick();
+      this.lastFrameTime = time;
     }
-
-    private startAnimation = () => {
-        this.lastFrameTime = 0;
-        this.frameId = window.requestAnimationFrame(this.playAnimation);
-    }
-
-    private playAnimation = (time: number) => {
-        this.update(time);
-        this.frameId = window.requestAnimationFrame(this.playAnimation);
-    }
-
-    private stopAnimation = () => {
-        if (this.frameId) {
-            window.cancelAnimationFrame(this.frameId);
-            this.frameId = 0;
-        }
-    }
-
-    private tick = () => {
-        if (this.callback) {
-            this.callback();
-        }
-    }
-
-    private update = (time: number) => {
-        if (time - this.lastFrameTime >= this.frameDuration) {
-            this.tick();
-            this.lastFrameTime = time;
-        }
-    }
+  };
 }
 
 export function createFrameRateManager() {
-    return new FrameRateManagerClass();
+  return new FrameRateManagerClass();
 }
